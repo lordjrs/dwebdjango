56680f1ad66e30b84c0dd1a13c2c5d2118137c01
Yared <yar.ordenes@alumnos.duoc.cl>
2020-10-28 21:29:19 -0300

    #002 home corriendo

--- src/dweb2/urls.py
+++ src/dweb2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from poleras import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home.html')
+
 ]
--- src/poleras/views.py
+++ src/poleras/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def about(request):
+    context={
+
+    }
+    return render(request, "about.html", context)
+
+def carrito(request):
+    context={
+
+    }
+    return render(request, "Carrito.html", context)
+
+def home(request):
+    context={
+
+    }
+    return render(request, "home.html", context)
+
+def layout(request):
+    context={
+
+    }
+    return render(request, "layout.html", context)
+
+def register(request):
+    context={
+
+    }
+    return render(request, "register.html", context)
